59cd523a9288f5d66c547abe55a32521486ed19b
Кирилл Храмов <hramovkirill01@gmail.com>
2023-01-21 15:29:33 +0300

    - Edit .env and frontend

--- src/Controller/RoofController.php
+++ src/Controller/RoofController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Coating;
 use App\Entity\MaterialType;
 use App\Repository\CoatingRepository;
 use App\Repository\MaterialTypeRepository;
+use Symfony\Bridge\Doctrine\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -22,13 +24,10 @@ class RoofController extends AbstractController
     #[Route('/roof', name: 'app_roof')]
     public function index(CoatingRepository $coatingRepository, MaterialTypeRepository $materialTypeRepository): Response
     {
+
         return new Response($this->twig->render('roof/index.html.twig', [
            'coatings' => $coatingRepository->findAll(),
-            'materials' => $materialTypeRepository->findAll()
-
-
-
-            // 'controller_name' => 'RoofController',
+            'materials' => $materialTypeRepository->findAll(),
         ]));
     }
 
