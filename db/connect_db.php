a8a7be800e34e13e379b078c60a1684ca45cbd2e
Кирилл Храмов <hramovkirill01@gmail.com>
2023-01-22 14:08:55 +0300

    - Рабочее подключение к бд через PDO и встроенные команды postgresql

--- db/connect_db.php
+++ db/connect_db.php
@@ -1,26 +1,37 @@
 <?php
 
-$loader = new \Twig\Loader\FilesystemLoader('/path/to/templates');
-$twig = new \Twig\Environment($loader, [
-    'cache' => '/path/to/compilation_cache',
-]);
+/*
+$user = 'main';
+$password = 'main';
+$host = 'localhost';
+$dbname = 'app';
+$port = 65341;
 
-$dbc = pg_connect("host=localhost port=63226 dbname=app user=main password=main");
-
-if (!$dbc) {
-    echo "Error.\n";
+//$dsn = 'pgsql:dbname=app;host=localhost';
+$dsn = "pgsql:host=".$host.";port=".$port.";dbname=".$dbname;
+try {
+    $db = new PDO($dsn,$user,$password);
 }
-$res = pg_query($dbc, "SELECT title FROM coating");
+catch(PDOException $e) {
+    die($e->getMessage());
+}
+
+$sql = 'SELECT * FROM coating';
+foreach ($db->query($sql) as $row){
+    echo $row['id'] . "\t";
+    echo $row['title']. "\t";
+}
+*/
+
+$db = pg_connect("host=127.0.0.1 port=65341 dbname=app user=main password=main")
+or die('Не удалось соединиться: ' . pg_last_error());
+
+$res = pg_query($db, "SELECT * FROM coating");
 if (!$res) {
-    echo "Ошибка вывода данных";
+   echo "Ошибка вывода данных";
 }
 
 $rows = pg_fetch_assoc($res);
+var_dump($rows);
 
-$template = $twig->load('index.html.twig');
-
-echo $template->render(array(
-    'id' => $rows['id'],
-    'title' => $rows['title'],
-));
 
