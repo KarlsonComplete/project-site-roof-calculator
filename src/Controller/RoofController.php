1752d4a3b6ace43b1a0498017677e17248ad2d45
Кирилл Храмов <hramovkirill01@gmail.com>
2023-01-25 14:00:47 +0300

    - Обновлен ajax маршрутизатор

--- src/Controller/RoofController.php
+++ src/Controller/RoofController.php
@@ -12,7 +12,6 @@ use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
 
-
 class RoofController extends AbstractController
 {
     private Environment $twig;
@@ -25,7 +24,8 @@ class RoofController extends AbstractController
     #[Route('/roof', name: 'app_roof')]
     public function index(CoatingRepository $coatingRepository, MaterialTypeRepository $materialTypeRepository, TypeOfSelectMaterialRepository $typeOfSelectMaterialRepository): Response
     {
-        if(!empty($_GET['rows'])){
+
+        if (!empty($_GET['rows'])) {
             return new Response($this->twig->render('roof/index.html.twig', [
                 'rows' => $_GET['rows'],
                 'types' => null,
@@ -33,7 +33,7 @@ class RoofController extends AbstractController
             ]));
         }
 
-        if (empty($_GET['rows'])){
+        if (empty($_GET['rows'])) {
             return new Response($this->twig->render('roof/index.html.twig', [
                 'rows' => null,
                 'types' => null,
@@ -41,14 +41,14 @@ class RoofController extends AbstractController
             ]));
         }
 
-        if(!empty($_GET['types'])){
+        if (!empty($_GET['types'])) {
             return new Response($this->twig->render('roof/index.html.twig', [
                 'types' => $_GET['types'],
                 'coatings' => $coatingRepository->findAll(),
             ]));
         }
 
-        if (empty($_GET['types'])){
+        if (empty($_GET['types'])) {
             return new Response($this->twig->render('roof/index.html.twig', [
                 'types' => null,
                 'coatings' => $coatingRepository->findAll(),
@@ -58,33 +58,26 @@ class RoofController extends AbstractController
     }
 
     #[Route('/roof/ajax', name: 'app_roof_ajax')]
-    public function ajax(Request $request, MaterialTypeRepository $materialTypeRepository, TypeOfSelectMaterialRepository $typeOfSelectMaterialRepository, CoatingRepository $coatingRepository):Response
+    public function ajax(Request $request, MaterialTypeRepository $materialTypeRepository, TypeOfSelectMaterialRepository $typeOfSelectMaterialRepository, CoatingRepository $coatingRepository): Response
     {
-            $rows = null;
-            $types = null;
-      //  $selected = $request->query->get('action');
-     //   if ($request->isXmlHttpRequest()) {
-            switch ($_POST['action']) {
-                case "showMaterialForInsert":
-                  $rows = $materialTypeRepository->SearchForIdenticalId($_POST['id_coating']);
+        $rows = null;
+        $types = null;
+        $selected= $request->getContent();
+        parse_str($selected,$output);
+        switch ($output['action']) {
+            case "showMaterialForInsert":
+                $rows = $materialTypeRepository->SearchForIdenticalId($output['id_coating']);
 
                 break;
-                case "showCityForInsert":
-                    $types = $typeOfSelectMaterialRepository->SearchForIdenticalId($_POST['id_material']);
+            case "showCityForInsert":
+                $types = $typeOfSelectMaterialRepository->SearchForIdenticalId($output['id_material']);
 
                 break;
-            }
-     //   }
+        }
 
-         /*   return new Response($this->twig->render('roof/ajax.html.twig', [
-                'rows' => $rows,
-                'coatings' => $coatingRepository->findAll(),
-            ]));
-            */
 
-            return $this->redirectToRoute('app_roof', ['rows' => $rows,'types'=> $types]);
+        return $this->redirectToRoute('app_roof', ['rows' => $rows, 'types' => $types]);
 
     }
-
 
 }
