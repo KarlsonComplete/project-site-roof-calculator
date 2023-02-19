321b2ff0a59f433ebeadb2fba77ed846ee454809
Кирилл Храмов <hramovkirill01@gmail.com>
2023-02-19 14:02:28 +0300

    - Добавили еще 2 функции в класс для расчета данных крыши.

--- templates/script.js
+++ templates/script.js
@@ -33,14 +33,12 @@ function MaterialChange() {
     }
 }
 
-/*function roof1(input1_1 , input2_1){
-   return  input1_1 * input2_1;
-}*/
 function calculate() {
 
     var sel = document.getElementById("primer");
     var type = sel.options[sel.selectedIndex].value;
     var text = sel.options[sel.selectedIndex].text;
+    var calculator = new Calculator();
     var result;
     console.log(type)
 
@@ -48,33 +46,26 @@ function calculate() {
         case 'roof1':
             var input1_1 = Number(document.getElementById('val_p1_1').value);
             var input2_1 = Number(document.getElementById('val_p1_2').value);
-            var test = new Calculator();
-            result = test.roof1(input1_1,input2_1)
+            result = calculator.roof1(input1_1,input2_1)
             break;
         case 'roof2':
             var input1_2 = Number(document.getElementById('val_p2_1').value);
             var input2_2 = Number(document.getElementById('val_p2_2').value);
             var input3_2 = Number(document.getElementById('val_p2_3').value);
-            result = (input1_2 * input2_2) + (input1_2 * input3_2)
+            result = calculator.roof2(input1_2,input2_2,input3_2)
             break;
         case 'roof3':
+            var input1_3 = Number(document.getElementById('val_p3_1').value);
+            var input2_3 = Number(document.getElementById('val_p3_2').value);
+            var input3_3 = Number(document.getElementById('val_p3_3').value);
+            result = calculator.roof3(input1_3, input2_3, input3_3)
             break;
         case 'roof4':
             var input1_4 = Number(document.getElementById('val_p4_1').value);
             var input2_4 = Number(document.getElementById('val_p4_2').value);
             var input3_4 = Number(document.getElementById('val_p4_3').value);
             var input4_4 = Number(document.getElementById('val_p4_4').value);
-            var sum = input2_4 + input3_4 + input2_4
-            var p_triangle = sum / 2;
-            var s_triangle = Math.sqrt((p_triangle * (p_triangle - input2_4) * (p_triangle - input2_4) * (p_triangle - input3_4)));
-            var p_trap = (input1_4 + input2_4 + input3_4 + input4_4) / 2;
-            if (input1_4 === input4_4) {
-                var s_square = input2_4 * input4_4;
-                result = (s_square + s_triangle) * 2;
-            } else {
-                var s_trap = ((input1_4 + input4_4) / (Math.abs(input1_4 - input4_4))) * (Math.sqrt((p_trap - input4_4) * (p_trap - input1_4) * (p_trap - input4_4 - input2_4) * (p_trap - input4_4 - input2_4)))
-                result = (s_trap + s_triangle) * 2
-            }
+            result = calculator.roof4(input1_4,input2_4,input3_4,input4_4)
             break;
     }
     document.getElementById("result").style.display = "block";
